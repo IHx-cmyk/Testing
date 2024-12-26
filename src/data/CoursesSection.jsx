af3fa9570f6b21bea3ac74a0363e3094a77caa4f
IHx-cmyk <najihko1@gmail.com>
2024-12-26 21:57:20 +0700

    Update CoursesSection.jsx

--- src/data/CoursesSection.jsx
+++ src/data/CoursesSection.jsx
@@ -1,6 +1,6 @@
 export const coursesSection = {
     content: `<p className="deskripsi">README 🙏</p>
-                        <h2>Tentang Tengnologi</h2>Teknologi adalah penerapan pengetahuan ilmiah dan kemampuan untuk menciptakan, mengembangkan, dan memanfaatkan alat, sistem, atau proses yang dapat mempermudah kehidupan manusia.</p>
+                        <h2>Tentang Teknologi</h2>Teknologi adalah penerapan pengetahuan ilmiah dan kemampuan untuk menciptakan, mengembangkan, dan memanfaatkan alat, sistem, atau proses yang dapat mempermudah kehidupan manusia.</p>
                         <p>Dengan Adanya Teknologi Sekarang Anda Bisa Belajar Melalui Platform Mana Pun Seperti YouTube, Jadi Gunakan Lah Teknologi Dengan Sebaik Baiknya👌.</p>
                         <p><a href="" className="tbl-biru">Pelajari Lebih Lanjut</a></p>`,
     image: 'https://img.freepik.com/free-vector/online-learning-isometric-concept_1284-17947.jpg?size=626&ext=jpg&ga=GA1.2.1769275626.1605867161'
