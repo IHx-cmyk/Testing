c7bf6724f96ff0f63161d59f431c70384bed846c
IHx-cmyk <najihko1@gmail.com>
2024-12-26 20:46:17 +0700

    first commit

---
+++ src/data/ContactSection.jsx
@@ -0,0 +1,19 @@
+export const contactSection = [
+    {
+        content: `<h3>Pesan</h3>
+                        <p>Belajar lah mengahrgai, Bukan menyakiti</p>`
+    },
+    {
+        content: `<h3>Tentang</h3>
+                        <p>Saya Hanya Seorang Anak Yang Lahir Dari Keluarga Sederhana Dan Mempunyai Cita-Cita Yabg Luar Biasa</p>`
+    },
+    {
+        content: `<h3>Kontak</h3>
+                        <p> Dsn. Telogo Rejo, Ds. Gampang Sejati, Kec. Laren, Kab. Lamongan</p>
+                        <p>Kode Pos: 62262</p>`
+    },
+    {
+        content: `<h3>Kritik & Saran</h3>
+                        <p><b>WhatsApp: </b>+62 881-8471-170</p>`
+    },
+]
